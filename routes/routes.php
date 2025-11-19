83d75ef219e42f6760ec074bb1205e99cbb1b6d7
andersonfarias-collab <anderson.farias@gmail.com>
2025-11-18 23:18:59 -0300

    Fix JSON encoding for /ping route response

--- routes/routes.php
+++ routes/routes.php
@@ -2,8 +2,20 @@
 use Slim\App;
 
 return function (App $app) {
-    $app->get('/ping', function ($req, $res) {
-        $res->getBody()->write(json_encode(['pong'=>true]));
+
+    // Rota principal (GET /)
+    $app->get('/', function ($req, $res) {
+        $res->getBody()->write(json_encode([
+            'status' => 'ok',
+            'message' => 'API-NFe rodando'
+        ]));
         return $res->withHeader('Content-Type','application/json');
     });
+
+    // Rota de teste (GET /ping)
+    $app->get('/ping', function ($req, $res) {
+        $res->getBody()->write(json_encode(['pong' => true]));
+        return $res->withHeader('Content-Type','application/json');
+    });
+
 };
