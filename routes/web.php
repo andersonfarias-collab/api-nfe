4a2bcbfe77161f61dd82e361b35fc3860779a7d1
andersonfarias-collab <anderson.farias@gmail.com>
2025-11-18 22:49:59 -0300

    Delete routes directory

--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Slim\App;
-
-return function (App $app) {
-    $app->get('/ping', function ($req, $res) {
-        $res->getBody()->write(json_encode(['pong'=>true]));
-        return $res->withHeader('Content-Type','application/json');
-    });
-};
